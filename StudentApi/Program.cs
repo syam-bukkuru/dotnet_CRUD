102b8b8b438f9dc800e77390668b5d582aba51ce
syam-bukkuru <syambukkuru@gmail.com>
2025-11-22 14:43:34 +0530

    final changes

--- StudentApi/Program.cs
+++ StudentApi/Program.cs
@@ -34,7 +34,8 @@ builder.Services.AddSwaggerGen(options =>
 
 // Register DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
