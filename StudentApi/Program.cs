c5e430dbcd5b15e4d9c8dae1596427c9410d609e
syam-bukkuru <syambukkuru@gmail.com>
2025-11-23 20:58:31 +0530

    Refactor CORS and Swagger setup in Program.cs
    Refactored CORS configuration and Swagger setup. Removed redundant CORS code and adjusted middleware order for better clarity.

--- StudentApi/Program.cs
+++ StudentApi/Program.cs
@@ -4,6 +4,19 @@ using StudentApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var frontendUrl = "https://mango-sand-058a21a00.3.azurestaticapps.net";
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(frontendUrl)
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Controllers
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -11,27 +24,9 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.PropertyNamingPolicy = null;
     });
 
-// CORS (Allow ONLY your frontend)
-var frontendUrl = "https://mango-sand-058a21a00.3.azurestaticapps.net";
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-        policy => policy.WithOrigins(frontendUrl)
-                        .AllowAnyHeader()
-                        .AllowAnyMethod());
-});
-
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Student API",
-        Version = "v1"
-    });
-});
+builder.Services.AddSwaggerGen();
 
 // SQLite DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -39,27 +34,26 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-// Run Migrations Automatically
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();       // <---- IMPORTANT
-}
+// MUST BE FIRST MIDDLEWARE after building
+app.UseCors("AllowFrontend");
 
+// Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Disable HTTPS redirect except local
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpsRedirection();
-}
-
-// Apply CORS policy
-app.UseCors("AllowFrontend");
+// Azure needs HTTPS redirection DISABLED completely
+// Comment this out for production:
+//// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Auto migrate
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
